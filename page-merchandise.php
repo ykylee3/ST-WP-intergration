14f4f4b6cfeb561777ba2e63094bf5abcab2cd77
ykylee3 <37039681+ykylee3@users.noreply.github.com>
2018-03-24 20:05:24 +0000

    added animation for spaceship

--- page-merchandise.php
+++ page-merchandise.php
@@ -7,12 +7,112 @@
   <div class="jumbotron-fluid">
     <div class="container-fluid">
       <div class="row">
-            <div id="particles-js">
-              <div class="jumbotron-fluid">
-                <div class="enterprise float-right">
-                  <img src="<?php echo get_template_directory_uri();?>/assets/enterprise-02-s.png">
-                </div>
+        <div id="particles-js">
+          <div class="jumbotron-fluid">
+            <div id="row-1-col-1" class="positioner"></div>
+            <div id="row-1-col-2" class="positioner"></div>
+            <div id="row-1-col-3" class="positioner"></div>
+            <div id="row-1-col-4" class="positioner"></div>
+            <div id="row-1-col-5" class="positioner"></div>
+            <div id="row-1-col-6" class="positioner"></div>
+            <div id="row-1-col-7" class="positioner"></div>
+            <div id="row-1-col-8" class="positioner"></div>
+            <div id="row-1-col-9" class="positioner"></div>
+            <div id="row-1-col-10" class="positioner"></div>
+            <div id="row-2-col-1" class="positioner"></div>
+            <div id="row-2-col-2" class="positioner"></div>
+            <div id="row-2-col-3" class="positioner"></div>
+            <div id="row-2-col-4" class="positioner"></div>
+            <div id="row-2-col-5" class="positioner"></div>
+            <div id="row-2-col-6" class="positioner"></div>
+            <div id="row-2-col-7" class="positioner"></div>
+            <div id="row-2-col-8" class="positioner"></div>
+            <div id="row-2-col-9" class="positioner"></div>
+            <div id="row-2-col-10" class="positioner"></div>
+            <div id="row-3-col-1" class="positioner"></div>
+            <div id="row-3-col-2" class="positioner"></div>
+            <div id="row-3-col-3" class="positioner"></div>
+            <div id="row-3-col-4" class="positioner"></div>
+            <div id="row-3-col-5" class="positioner"></div>
+            <div id="row-3-col-6" class="positioner"></div>
+            <div id="row-3-col-7" class="positioner"></div>
+            <div id="row-3-col-8" class="positioner"></div>
+            <div id="row-3-col-9" class="positioner"></div>
+            <div id="row-3-col-10" class="positioner"></div>
+            <div id="row-4-col-1" class="positioner"></div>
+            <div id="row-4-col-2" class="positioner"></div>
+            <div id="row-4-col-3" class="positioner"></div>
+            <div id="row-4-col-4" class="positioner"></div>
+            <div id="row-4-col-5" class="positioner"></div>
+            <div id="row-4-col-6" class="positioner"></div>
+            <div id="row-4-col-7" class="positioner"></div>
+            <div id="row-4-col-8" class="positioner"></div>
+            <div id="row-4-col-9" class="positioner"></div>
+            <div id="row-4-col-10" class="positioner"></div>
+            <div id="row-5-col-1" class="positioner"></div>
+            <div id="row-5-col-2" class="positioner"></div>
+            <div id="row-5-col-3" class="positioner"></div>
+            <div id="row-5-col-4" class="positioner"></div>
+            <div id="row-5-col-5" class="positioner"></div>
+            <div id="row-5-col-6" class="positioner"></div>
+            <div id="row-5-col-7" class="positioner"></div>
+            <div id="row-5-col-8" class="positioner"></div>
+            <div id="row-5-col-9" class="positioner"></div>
+            <div id="row-5-col-10" class="positioner"></div>
+            <div id="row-6-col-1" class="positioner"></div>
+            <div id="row-6-col-2" class="positioner"></div>
+            <div id="row-6-col-3" class="positioner"></div>
+            <div id="row-6-col-4" class="positioner"></div>
+            <div id="row-6-col-5" class="positioner"></div>
+            <div id="row-6-col-6" class="positioner"></div>
+            <div id="row-6-col-7" class="positioner"></div>
+            <div id="row-6-col-8" class="positioner"></div>
+            <div id="row-6-col-9" class="positioner"></div>
+            <div id="row-6-col-10" class="positioner"></div>
+            <div id="row-7-col-1" class="positioner"></div>
+            <div id="row-7-col-2" class="positioner"></div>
+            <div id="row-7-col-3" class="positioner"></div>
+            <div id="row-7-col-4" class="positioner"></div>
+            <div id="row-7-col-5" class="positioner"></div>
+            <div id="row-7-col-6" class="positioner"></div>
+            <div id="row-7-col-7" class="positioner"></div>
+            <div id="row-7-col-8" class="positioner"></div>
+            <div id="row-7-col-9" class="positioner"></div>
+            <div id="row-7-col-10" class="positioner"></div>
+            <div id="row-8-col-1" class="positioner"></div>
+            <div id="row-8-col-2" class="positioner"></div>
+            <div id="row-8-col-3" class="positioner"></div>
+            <div id="row-8-col-4" class="positioner"></div>
+            <div id="row-8-col-5" class="positioner"></div>
+            <div id="row-8-col-6" class="positioner"></div>
+            <div id="row-8-col-7" class="positioner"></div>
+            <div id="row-8-col-8" class="positioner"></div>
+            <div id="row-8-col-9" class="positioner"></div>
+            <div id="row-8-col-10" class="positioner"></div>
+            <div id="row-9-col-1" class="positioner"></div>
+            <div id="row-9-col-2" class="positioner"></div>
+            <div id="row-9-col-3" class="positioner"></div>
+            <div id="row-9-col-4" class="positioner"></div>
+            <div id="row-9-col-5" class="positioner"></div>
+            <div id="row-9-col-6" class="positioner"></div>
+            <div id="row-9-col-7" class="positioner"></div>
+            <div id="row-9-col-8" class="positioner"></div>
+            <div id="row-9-col-9" class="positioner"></div>
+            <div id="row-9-col-10" class="positioner"></div>
+            <div id="row-10-col-1" class="positioner"></div>
+            <div id="row-10-col-2" class="positioner"></div>
+            <div id="row-10-col-3" class="positioner"></div>
+            <div id="row-10-col-4" class="positioner"></div>
+            <div id="row-10-col-5" class="positioner"></div>
+            <div id="row-10-col-6" class="positioner"></div>
+            <div id="row-10-col-7" class="positioner"></div>
+            <div id="row-10-col-8" class="positioner"></div>
+            <div id="row-10-col-9" class="positioner"></div>
+            <div id="row-10-col-10" class="positioner"></div>
+            <div class="enterprise img-fluid mx-auto d-block">
+              <img class="" src="<?php echo get_template_directory_uri();?>/assets/enterprise-02-s.png">
             </div>
+          </div>
         </div>
       </div>
     </div>
