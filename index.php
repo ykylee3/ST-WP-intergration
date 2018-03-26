49c9356def7eab5696055219b3762a2666597ad9
bebisamji <37157889+bebisamji@users.noreply.github.com>
2018-03-26 14:08:14 +0100

    Buttons link

--- index.php
+++ index.php
@@ -21,14 +21,14 @@
             Our aim is to bring the universe of Star Trek to new and old
             fans through events, screenings, our website, Facebook page
             and Twitter account.</p>
-            <p><a class="btn btn-primary" href="#" role="button" style="background: #FF4343; border-color: #5d5d5d">Learn more</a></p>
+            <p><a class="btn btn-primary" href="about.html" role="button" style="background: #FF4343; border-color: #5d5d5d">Learn more</a></p>
           </div>
         </div>
         <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
           <div class="clubhouse text-white">
             <h1 class="index_title"><a href="about.html">The Clubhouse</a></h1>
             <p>The clubhouse is our charity partner.</p>
-            <p><a class="btn btn-primary" href="#" role="button" style="background: #5d5d5d; border-color: #5d5d5d">Learn More</a></p>
+            <p><a class="btn btn-primary" href="about.html" role="button" style="background: #5d5d5d; border-color: #5d5d5d">Learn More</a></p>
           </div>
         </div>
       </div>
